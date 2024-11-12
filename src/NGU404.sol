e799dc51143f42feca9ab7c700c419ec0c75ef43
Mike Cherneski <mgcherneski@gmail.com>
2024-11-12 11:14:00 +0700

    Troubleshooting issues with solc versions.

--- src/NGU404.sol
+++ src/NGU404.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import {INGU404} from "./interfaces/INGU404.sol";
 import {ERC721Events} from "./lib/ERC721Events.sol";
